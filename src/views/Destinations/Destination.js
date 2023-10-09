12268c109203b3baca5171ccb842e36e3b12f485
Yash Chaudhari <120511697+yashc2003@users.noreply.github.com>
2023-10-09 13:30:18 +0000

    create a destination folder

---
+++ src/views/Destinations/Destination.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Navbar.js"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight} from '@fortawesome/free-solid-svg-icons'
+
+
+
+function Destination ()  {
+    return(
+        <div className='max-w-{1240px} mx-auto py-16 px-4 text-center'>
+            <h1>Destination</h1>
+        </div>
+    )
+    
+}
