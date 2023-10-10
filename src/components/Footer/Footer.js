fcf63981a7807310d3dd7130cbf1b541462f96b0
JayantSarve26 <jayantsarve707@gmail.com>
2023-10-10 19:55:59 +0530

    added footer section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,28 +1,17 @@
 import React from 'react';
 import "./Footer.css"
 
-const Footer = () => {
+export default function Footer() {
 
     return (
         <div className="footer">
             <div className='sb_footer section_padding'>
                 <div className='sb_footer-links'>
-                    <div className='sb_footer-links-div'>
-                        <h4>For Business</h4>
-                        <a href=" ">
-                            <p>Employer</p>
-                        </a>
-                        <a href=" ">
-                            <p>Employer</p>
-                        </a>
-                        <a href=" ">
-                            <p>Employer</p>
-                        </a>
-                    </div>
+                
                 </div>
             </div>
         </div>
     )
 }
-export default Contact
+
 
