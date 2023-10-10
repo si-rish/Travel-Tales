3c498366e184f418146459e412064e201f441c5e
JayantSarve26 <jayantsarve707@gmail.com>
2023-10-10 18:04:48 +0530

    added image folder

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import "./Footer.css"
+
+const Footer = () => {
+
+    return (
+        <div className="footer">
+            <div className='sb_footer section_padding'>
+                <div className='sb_footer-links'>
+                    <div className='sb_footer-links-div'>
+                        <h4>For Business</h4>
+                        <a href=" ">
+                            <p>Employer</p>
+                        </a>
+                        <a href=" ">
+                            <p>Employer</p>
+                        </a>
+                        <a href=" ">
+                            <p>Employer</p>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Contact
+
