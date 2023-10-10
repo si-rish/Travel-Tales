6d9e28c90611f680c22831d5aca768aafd939cb3
JayantSarve26 <jayantsarve707@gmail.com>
2023-10-10 20:06:27 +0530

    added more data in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
         <div className="footer">
             <div className='sb_footer section_padding'>
                 <div className='sb_footer-links'>
-                <div className='sb_footer-links-div'>
+                    <div className='sb_footer-links-div'>
                         <h4>In the Main Section </h4>
                         <a href=" ">
                             <p>Home</p>
@@ -21,6 +21,52 @@ export default function Footer() {
                         <a href=" ">
                             <p>Contact Us</p>
                         </a>
+                    </div>
+                    <div className="sb_footer-links-div">
+                        <h4>Resources</h4>
+                        <a href=" ">
+                            <p>Employer</p>
+                        </a>
+                        <a href=" ">
+                            <p>About</p>
+                        </a>
+                        <a href=" ">
+                            <p>individual</p>
+                        </a>
+                        <a href=" ">
+                            <p>Help</p>
+                        </a>
+                    </div>
+                    <div className="sb_footer-links-div">
+                        <h4>Company</h4>
+                        <a href="/RTC">
+                            <p>About Us</p>
+                        </a>
+                        <a href="/RTC">
+                            <p>Privacy Policy</p>
+                        </a>
+                        <a href="/RTC">
+                            <p>Copyright Ploicies</p>
+                        </a>
+                        <a href="/RTC">
+                            <p>Support</p>
+                        </a>
+                    </div>
+                    <div className="sb_footer-links-div">
+                        <h4>LEGAL POLICIES</h4>
+                        <a href=" ">
+                            <p>Terms & Conditions</p>
+                        </a>
+                        <a href=" ">
+                            <p>Disclaimer</p>
+                        </a>
+                        <a href=" ">
+                            <p>Privacy Policy</p>
+                        </a>
+                        <a href=" ">
+                            <p>Share Your Travel Experience</p>
+                        </a>
+
                     </div>
                 </div>
             </div>
