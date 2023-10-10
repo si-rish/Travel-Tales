96d06f8b71b61fd655ea8b1032007b5ad321b792
JayantSarve26 <jayantsarve707@gmail.com>
2023-10-10 19:59:12 +0530

    added footer content

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,7 +7,21 @@ export default function Footer() {
         <div className="footer">
             <div className='sb_footer section_padding'>
                 <div className='sb_footer-links'>
-                
+                <div className='sb_footer-links-div'>
+                        <h4>In the Main Section </h4>
+                        <a href=" ">
+                            <p>Home</p>
+                        </a>
+                        <a href=" ">
+                            <p>About Us</p>
+                        </a>
+                        <a href=" ">
+                            <p>latest News & Blog </p>
+                        </a>
+                        <a href=" ">
+                            <p>Contact Us</p>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
