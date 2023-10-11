c6776346d5f2ffa31bb33ac5d8b8a776f9f79378
Shanzey22 <shanzeyshaikh22@gmail.com>
2023-10-11 15:15:08 +0530

    const value given

--- src/views/Signup/Register.js
+++ src/views/Signup/Register.js
@@ -42,3 +42,12 @@ const Register = () => {
     useEffect(() => {
         setErrMsg('');
     }, [user, pwd, matchPwd])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        // if button enabled with JS hack
+        const v1 = USER_REGEX.test(user);
+        const v2 = PWD_REGEX.test(pwd);
+        if (!v1 || !v2) {
+            setErrMsg("Invalid Entry");
+            return;
