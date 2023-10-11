28ee6e6a8afcc3e9a7e8ba4f5ade03b0803b0150
Shanzey22 <shanzeyshaikh22@gmail.com>
2023-10-11 15:11:49 +0530

    const put in register page which is in sign page

--- src/views/Signup/Register.js
+++ src/views/Signup/Register.js
@@ -2,3 +2,11 @@ import { useRef, useState, useEffect } from "react";
 import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from './../../api/axios';
+
+const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+const REGISTER_URL = '/register';
+
+const Register = () => {
+    const userRef = useRef();
+    const errRef = useRef();
