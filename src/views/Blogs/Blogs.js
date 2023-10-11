ae8206c6a97cd30c4000ebbb4a338749906af1b5
prerna-barde26git config --global user.name <prernabarde98@gmail.com>
2023-10-12 02:02:04 +0530

    use state and map function

--- src/views/Blogs/Blogs.js
+++ src/views/Blogs/Blogs.js
@@ -1,8 +1,36 @@
+import React, {useState} from 'react'
 import "./Blogs.css"
 import Navbar from '../../components/Navbar/Navbar'
 import Footer from '../../components/Footer/Footer'
 
 export default function Blogs()  {
+    const[Blogs,setBlogs]=React.useState([
+      {
+        name:'Shreya',
+        date:11/10/2023,
+        Blog:'Out of Town Blog is a travel, food, and lifestyle online magazThis trave Out of Town Blog is a travel, food, and lifestyle online magazine. This travel blog started as a site where the publisher shares his personal travel experiences, photographs and travel tips to his friends, relatives, and online readers. Today, Out of Town Blog showcases travel tales, food reviews, hotel guides and reviews, travel destination guides and other travel-related inputs from various contributors from all over the world.',
+      },
+      {
+        name:'Shreya',
+        date:11/10/2023,
+        Blog:'Out of Town Blog is a travel, food, and lifestyle online magazThis travel Out of Town Blog is a travel, food, and lifestyle online magazine. This travel blog started as a site where the publisher shares his personal travel experiences, photographs and travel tips to his friends, relatives, and online readers. Today, Out of Town Blog showcases travel tales, food reviews, hotel guides and reviews, travel destination guides and other travel-related inputs from various contributors from all over the world.',
+      },
+      {
+        name:'Shreya',
+        date:11/10/2023,
+        Blog:'Out of Town Blog is a travel, food, and lifestyle online magazThis travelOut of Town Blog is a travel, food, and lifestyle online magazine. This travel blog started as a site where the publisher shares his personal travel experiences, photographs and travel tips to his friends, relatives, and online readers. Today, Out of Town Blog showcases travel tales, food reviews, hotel guides and reviews, travel destination guides and other travel-related inputs from various contributors from all over the world.',
+      },
+      {
+        name:'Shreya',
+        date:11/10/2023,
+        Blog:'Out of Town Blog is a travel, food, and lifestyle online magazThis travelOut of Town Blog is a travel, food, and lifestyle online magazine. This travel blog started as a site where the publisher shares his personal travel experiences, photographs and travel tips to his friends, relatives, and online readers. Today, Out of Town Blog showcases travel tales, food reviews, hotel guides and reviews, travel destination guides and other travel-related inputs from various contributors from all over the world.', 
+      },
+    ]);
+
+
+
+
+
     return(
         <div>
             <Navbar/>
@@ -10,8 +38,20 @@ export default function Blogs()  {
             <div className="division1">
                <div className="division2">
                <h2 className="heading2">Inspiring Travel Blogs</h2>
-               <div className="division3">
-                 <p>hello</p>
+               <div >
+                 {
+                    Blogs.map((Blog,index)=>{
+                        
+                       return(
+                        <div className='BlogCard'>
+                            <p className='blog-name'>{Blog.name}</p>
+                            <p className='blog-name'>{Blog.date}</p>
+                            <p className='blog-name'>{Blog.blog}</p>
+                            </div>
+                       )
+                    })
+                    
+                 }
                </div>
             
             
