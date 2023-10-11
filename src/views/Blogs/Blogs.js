d7e9dc0b5ae964f029ca3238a7a0bdc2d3cc3fc2
prerna-barde26git config --global user.name <prernabarde98@gmail.com>
2023-10-11 05:58:46 +0530

    first commit

--- src/views/Blogs/Blogs.js
+++ src/views/Blogs/Blogs.js
@@ -5,7 +5,18 @@ export default function Blogs()  {
     return(
         <div>
             <Navbar/>
-            <h1>Blogss</h1>
+            <h2 className="heading1">To Travel 🧳 is to Live..!✈️</h2>
+            <div className="division1">
+               <div className="division2">
+               <h2 className="heading2">Inspiring Travel Blogs</h2>
+
+
+               </div>
+               <div className="division2">
+                <h2 className="heading2">Share Your Travel Experience With Us</h2>
+
+               </div>
+            </div>
         </div>
     )
     
