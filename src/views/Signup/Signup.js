5eb17b5b46d21c88cdb9925370d7ac4a70a148cf
Shanzey22 <shanzeyshaikh22@gmail.com>
2023-10-11 18:29:13 +0530

    all clear

---
+++ src/views/Signup/Signup.js
@@ -0,0 +1,12 @@
+import "./Signup.css"
+import Navbar from '../../components/Navbar/Navbar'
+
+export default function Signup()  {
+    return(
+        <div>
+            <Navbar/>
+            <h1>Signup</h1>
+        </div>
+    )
+    
+}
