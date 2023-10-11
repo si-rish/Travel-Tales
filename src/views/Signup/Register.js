63139dd4911e8c55c7a77225c498f4dd3dd8e7f2
Shanzey22 <shanzeyshaikh22@gmail.com>
2023-10-11 15:22:56 +0530

    add p tag for password

--- src/views/Signup/Register.js
+++ src/views/Signup/Register.js
@@ -138,4 +138,11 @@ const Register = () => {
                             aria-describedby="pwdnote"
                             onFocus={() => setPwdFocus(true)}
                             onBlur={() => setPwdFocus(false)}
-                        />     
+                        />  
+
+                          <p id="pwdnote" className={pwdFocus && !validPwd ? "instructions" : "offscreen"}>
+                            <FontAwesomeIcon icon={faInfoCircle} />
+                            8 to 24 characters.<br />
+                            Must include uppercase and lowercase letters, a number and a special character.<br />
+                            Allowed special characters: <span aria-label="exclamation mark">!</span> <span aria-label="at symbol">@</span> <span aria-label="hashtag">#</span> <span aria-label="dollar sign">$</span> <span aria-label="percent">%</span>
+                        </p>   
