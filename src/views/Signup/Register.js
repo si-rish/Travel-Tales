c7a77f7fd641a2f2746d3dc0ca6e9f5724c2e958
Shanzey22 <shanzeyshaikh22@gmail.com>
2023-10-11 15:25:47 +0530

    add button and form closing tag

--- src/views/Signup/Register.js
+++ src/views/Signup/Register.js
@@ -170,4 +170,5 @@ const Register = () => {
                             <FontAwesomeIcon icon={faInfoCircle} />
                             Must match the first password input field.
                         </p>
-  
+                        <button disabled={!validName || !validPwd || !validMatch ? true : false}>Sign Up</button>
+                    </form>
