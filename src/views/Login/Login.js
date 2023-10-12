8888bccb62603249fc6c05b975f91942014df167
prerna-barde26git config --global user.name <prernabarde98@gmail.com>
2023-10-12 06:17:24 +0530

    flexboxes updated

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -1,16 +1,1 @@
-<<<<<<< HEAD
 
-=======
-import "./Login.css"
-import Navbar from '../../components/Navbar/Navbar'
-
-export default function Login()  {
-    return(
-        <div>
-            <Navbar/>
-            <h1>Login</h1>
-        </div>
-    )
-    
-}
->>>>>>> 8805e27e69c445aebf43659857f2459fad4f199c
