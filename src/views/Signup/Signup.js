3cccebfe191e6ba43d2af63086e7d308911b331b
Shanzey22 <shanzeyshaikh22@gmail.com>
2023-10-12 14:16:02 +0530

    sign up pageand login page work completed .done!

--- src/views/Signup/Signup.js
+++ src/views/Signup/Signup.js
@@ -1,17 +1,27 @@
 import './Signup.css';
 import Registration from './../Signup/Registration';
-import '.././../../node_modules/bootstrap/dist/css/bootstrap.css'
+
+import '.././../../node_modules/bootstrap/dist/css/bootstrap.css';
+
 function Signup(){
   return (
+    <div>
+     <div className='background' 
+       >
     <div className='App'>
       <div className='outer'>
         <div className='inner'>
+          
           <Registration/>
+       
 
         </div>
         </div>
+
+    </div></div>
 
     </div>
+
   );
 }
 export default Signup;
