39702e8544bd823f6d7f44250937f32b966158a6
prerna-barde26git config --global user.name <prernabarde98@gmail.com>
2023-10-12 17:25:09 +0530

    additional design to flexboxes added

--- src/views/Blogs/Blogs.js
+++ src/views/Blogs/Blogs.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import "./Blogs.css"
 import Navbar from '../../components/Navbar/Navbar'
 import Footer from '../../components/Footer/Footer'
@@ -8,23 +8,33 @@ export default function Blog()  {
     const[blog,setBlogs]=React.useState([
       {
         name:'Shreya',
-        date:11/10/2023,
-        blog:'Out of Town Blog is a travel, food, and lifestyle online magazThis trave Out of Town Blog is a travel, food, and lifestyle online magazine. This travel blog started as a site where the publisher shares his personal travel experiences, photographs and travel tips to his friends, relatives, and online readers. Today, Out of Town Blog showcases travel tales, food reviews, hotel guides and reviews, travel destination guides and other travel-related inputs from various contributors from all over the world.',
+        date:11-6-2023,
+        blog:'=>Out of Town Blog is a travel, food, and lifestyle online magazThis trave Out of Town Blog is a travel, food, and lifestyle online magazine. This travel blog started as a site where the publisher shares his personal travel experiences, photographs and travel tips to his friends, relatives, and online readers. Today, Out of Town Blog showcases travel tales, food reviews, hotel guides and reviews, travel destination guides and other travel-related inputs from various contributors from all over the world.',
       },
       {
-        name:'Shreya',
-        date:11/10/2023,
-        blog:'Out of Town Blog is a travel, food, and lifestyle online magazThis travel Out of Town Blog is a travel, food, and lifestyle online magazine. This travel blog started as a site where the publisher shares his personal travel experiences, photographs and travel tips to his friends, relatives, and online readers. Today, Out of Town Blog showcases travel tales, food reviews, hotel guides and reviews, travel destination guides and other travel-related inputs from various contributors from all over the world.',
+        name:'Payal',
+        date:11-10-2023,
+        blog:'=>Out of Town Blog is a travel, food, and lifestyle online magazThis travel Out of Town Blog is a travel, food, and lifestyle online magazine. This travel blog started as a site where the publisher shares his personal travel experiences, photographs and travel tips to his friends, relatives, and online readers. Today, Out of Town Blog showcases travel tales, food reviews, hotel guides and reviews, travel destination guides and other travel-related inputs from various contributors from all over the world.',
       },
       {
-        name:'Shreya',
-        date:11/10/2023,
-        blog:'Out of Town Blog is a travel, food, and lifestyle online magazThis travelOut of Town Blog is a travel, food, and lifestyle online magazine. This travel blog started as a site where the publisher shares his personal travel experiences, photographs and travel tips to his friends, relatives, and online readers. Today, Out of Town Blog showcases travel tales, food reviews, hotel guides and reviews, travel destination guides and other travel-related inputs from various contributors from all over the world.',
+        name:'saru',
+        date:11-11-2023,
+        blog:'=>Out of Town Blog is a travel, food, and lifestyle online magazThis travelOut of Town Blog is a travel, food, and lifestyle online magazine. This travel blog started as a site where the publisher shares his personal travel experiences, photographs and travel tips to his friends, relatives, and online readers. Today, Out of Town Blog showcases travel tales, food reviews, hotel guides and reviews, travel destination guides and other travel-related inputs from various contributors from all over the world.',
       },
+     
      
     ]);
 
+    const [Count, setCount] = useState(0);
 
+  function showcountvalue() {
+  }
+  useEffect(showcountvalue, [Count])
+
+  function increase() {
+    setCount(Count + 1)
+
+  }
 
 
 
@@ -40,8 +50,11 @@ export default function Blog()  {
                        return (
                          <div className='BlogCard'>
                           <p className='blog-name m-2'>🎒{blog.name}</p>
-                          <p className='blog-date m-2'>📅{blog.date}</p>
+                          <date className='blog-date m-2'>📅{blog.date}</date>
                           <p className='blog-blog m-2'>🖼️{blog.blog}</p>
+                          <div className='like-division'>
+                            <div className='comment'>💬 Comment</div> <div className='like'>❤️1</div>
+                          </div>
                          </div>
                        )
 
@@ -68,6 +81,12 @@ export default function Blog()  {
 
                <div className="division4">
                 <h2 className="heading3">Share Your Travel Experience With Us..</h2>
+                <form>
+                
+                         
+
+                        
+                </form>
 
                </div>
             </div>
