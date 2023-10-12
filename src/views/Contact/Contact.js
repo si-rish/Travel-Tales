acd32c7fd6f6c4f387a4a8da65acb910dbaf2cee
JayantSarve26 <jayantsarve707@gmail.com>
2023-10-12 16:28:30 +0530

    added contact form code and css

--- src/views/Contact/Contact.js
+++ src/views/Contact/Contact.js
@@ -5,69 +5,25 @@ import Footer from './../../components/Footer/Footer';
 
 function Contact() {
     return (
-        <div>
+        <div >
             <Navbar />
-            <section className="contact">
-                <div className="content">
-                    <h1>Contact Us</h1>
-                    <p>We would love to hear from you. Please use the form below to send us a comment or ask a question. We will reply as soon as possible.</p>
-
-                </div>
-                <div className="container">
-                    <div className="contactInfo">
-                    <div className="box">
-                            <div className="icon"></div>
-                            <div className="text">
-                                <h3>Address</h3>
-                                <p>Plot No.50, Central Complex, Mount Road, 874974 </p>
-                            </div>
-                        </div>
-                      
-                        <div className="box">
-                            <div className="icon"></div>
-                            <div className="text">
-                                <h3>Phone</h3>
-                                <p>507-867-888 </p>
-                            </div>
-                        </div>
-                        <div className="box">
-                            <div className="icon"></div>
-                            <div className="text">
-                                <h3>Email</h3>
-                                <p>TravelsTales@gmail.com</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="contactForm">
-                        <form>
-                            <h2>send message</h2>
-                            <div className="inputBox">
-                                <input type="text" name="" required=""></input>
-                                <h2>Full Name</h2>
-                            </div>
-                            <div className="inputBox">
-                                <input type="text" name="" required=""></input>
-                                <h2>Email</h2>
-                            </div>
-                            <div className="inputBox">
-                                <input type="text" name="" required=""></input>
-                                <h2>Type your Message...</h2>
-                            </div>
-                            <div className="inputBox">
-                                <input type="submit" name="" vlaue="send"></input>
-
-                            </div>
-                        </form>
-
-
-                    </div>
-
-                </div>
-
-            </section>
+            <div className="contact-container">
+            
+            <form>
+                <h1>Contact for Enquiry Here...</h1>
+                <input type="text" name="name" id="" placeholder="Enter Name" className="form_input-box"></input>
+                <input type="email" name="emial" id="" placeholder="example@gmail.com" className="form_input-box"></input>
+                <input type="phone" name="phone" id="" placeholder="+91" className="form_input-box"></input>
+                <input type="message" name="message" id="" placeholder="Type Here Message........" className="form_input-msg-box"></input>
+                <button type="submit" className="submit-btn">
+                    Submit
+                </button>
+            </form>
+            </div>
 
             <Footer />
         </div>
+        
     )
 
 }
