4d2f5aa13110284d55b4d6368f99291d0e881bab
si-rish <shirishjmeshram@gmail.com>
2023-10-13 18:21:26 +0530

    update changes

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -8,7 +8,7 @@ function HeroSection() {
         <div className='hero-main-container'>
             <div className='hero-container'>
                 <h1 className='hero_section-title fade-in-bottom'>
-                <br/> <br/>Explore Travel Tales<br />Real Adventure & Travels</h1>
+                <br/> <br/>Explore Travel Tales<br />Real Adventure & Travels</h1> <br/>
                 <button className='herossection-btn'>Explore Now <FontAwesomeIcon icon={faArrowRight} className='hero-arrow' /></button>
             </div>
         </div>
