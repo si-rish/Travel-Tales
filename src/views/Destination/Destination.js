9427d3de24b6481b400c6fb7ea8dfa63ac6d1f66
si-rish <shirishjmeshram@gmail.com>
2023-10-15 06:52:35 +0530

    update destination page

--- src/views/Destination/Destination.js
+++ src/views/Destination/Destination.js
@@ -7,31 +7,44 @@ import CardSection from '../../components/CardSection/Cardsection'
 import CardSlider from '../../components/CardSlider/cardslider'
 import CardInfo from '../../components/CardInfo/cardinfo'
 import Footer from '../../components/Footer/Footer'
+import BookingForm from "../../components/BookingForm/BookingForm"
 
 export default function Destination() {
-    return(
+    return (
         <div>
-           <Navbar/>
-            <div className="header-main-img">
-            <h1 className="text-header fade-in-bottom"  ><b>Destination</b></h1>
-               
-           </div>
-         
-            <Form/>
-            <h1 className="title1"><b>Explore Real Adventure</b></h1>
-           <ImageGallery/>
-           <input className="search" type="searchbar" placeholder="Search..." ></input>
-              <h1 className="title2"><b>Top Trending Destinatins </b></h1>
-              <CardInfo/>
-              <h2 className="title3"><b>Explore Top Destination by Region</b></h2>
-            <CardSlider/> 
-            
-            <CardSection/>
+            <Navbar />
+            <div className="destination-main-img">
+                <h1 className="hero_section-title fade-in-bottom"  ><br /> <br /><br />Destinations </h1>
+
+            </div>
+
+            <p className='app-desc destination-app-desc'>Visit the breathtaking Taj Mahal in Agra, an exquisite symbol of love and architectural marvel. Explore the serene backwaters of Kerala for a tranquil escape amidst lush greenery and pristine waters.</p>
+
+            {/* <Form/> */}
+            <hr className="destination-hr" />
+            <BookingForm />
+            <h1 className="destination-descrpition description"><b>Explore Real Adventure</b></h1>
+            <p className="caption destination-caption">
+                A traveler is an individual who explores different places, cultures, and experiences, often seeking adventure and personal growth through their journeys. They possess a spirit of wanderlust and a curiosity for the world beyond their own borders.
+            </p>
+            <div class="search-container">
+                <input class="search-input" type="text" placeholder="Search" />
+                <button class="search-button">Search</button>
+            </div>
+
+        <hr className="destination-hr" />
+            <ImageGallery />
+            <h1 className="title2"><b>Top Trending Destinatins </b></h1>
+            <CardInfo />
+            <h2 className="title3"><b>Explore Top Destination by Region</b></h2>
+            <CardSlider />
+
+            <CardSection />
             <Footer />
         </div>
-)
+    )
 }
-    
-    
-    
+
+
+
 
