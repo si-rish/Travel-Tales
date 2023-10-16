e95417327237bb1a90f7968c646b4d76cd62edeb
Shanzey22 <shanzeyshaikh22@gmail.com>
2023-10-16 20:30:21 +0530

    changes

--- src/views/MainHome/MainHome.js
+++ src/views/MainHome/MainHome.js
@@ -26,13 +26,7 @@ function MainHome(){
 
 
   <div className="backgroundimage">
-  <iframe width="100%"
-   height="750px" 
-   src="https://www.youtube.com/embed/5BPz0rtKVco?si=5aqthjQlCwPgBnEm" 
-   title="YouTube video player" 
-   frameborder="0" 
-   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-    allowfullscreen></iframe>
+
 
     <br/>
     <iframe width="100%" height="750px" 
