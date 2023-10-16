88f6757ae0e8bd9c3ba85cbf7b1c3725a1b26136
Shanzey22 <shanzeyshaikh22@gmail.com>
2023-10-16 13:34:16 +0530

    added correct path from login to home page

--- src/views/MainHome/MainHome.js
+++ src/views/MainHome/MainHome.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./MainHome.css";
 import Footer from "../../components/Footer/Footer";
 import { useNavigate } from "react-router-dom";
+
+
 function MainHome(){
     const navigate = useNavigate();
   
@@ -13,20 +15,12 @@ function MainHome(){
       
     <>
     <div className="backgroundimage">
-     
-        
-          
-
-           
-                    <button onClick={signing} className="middle btn btn-warning   btn-lg btn-block  "
-                   
-                    >Sign up</button></div>
+      <button onClick={signing} className="middle btn btn-warning   btn-lg btn-block" >Sign up</button></div>
 
 
 <div className="mt-5 mx-5  mb-4 " >
 <h1>Why Tours and Travel</h1></div><br/>
 <div className="benefits mt-3 mb-5">
-
 
 
 </div>   
